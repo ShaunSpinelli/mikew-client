936aa5e23d17f0c133d80c070441b3a40d060ff8
Fraser Solomon <fmsolomon1@gmail.com>
2018-07-11 09:28:13 +1000

    fixing homepage

--- src/components/MakeBooking.js
+++ src/components/MakeBooking.js
@@ -50,31 +50,38 @@ class MakeBooking extends React.Component {
   render() {
     const {startTime, endTime, bookingButton, bookings} = this.state
     return (
-        <div className= "Makebooking">
-            <h3> what day? </h3>
-            <DatePicker
-                selected= {moment()}
-                onChange={this.handleDateChange}
-            />
-            <h3> what time? </h3>
-            <p> start </p> 
-            <TimePicker 
-                start={"7:00"}
-                end="16:00" 
-                step={30} 
-                value={startTime }
-                onChange={this.handleStartTimeChange}
-            />
-            <p> end </p> 
-            <TimePicker 
-                start={ this.timeConverter(startTime) || "7:30" } 
-                end="17:00" 
-                step={30}
-                value={endTime}
-                onChange={this.handleEndTimeChange}
-            />
-            <br />
-            <button disabled={bookingButton}  onClick={this.handleBookingRequest}> Make Booking request! </button>
+        <div>
+            <div className= "Makebooking">
+                <div className= "Makebooking--whatDate">
+                    <h3> what day? </h3>
+                    <DatePicker
+                        selected= {moment()}
+                        onChange={this.handleDateChange}
+                    />
+                </div>
+                <div className="Makebooking--whatTime">
+                    <h3> what time? </h3>
+                    <p> start </p> 
+                    <TimePicker 
+                        start={"7:00"}
+                        end="16:00" 
+                        step={30} 
+                        value={startTime }
+                        onChange={this.handleStartTimeChange}
+                    />
+                    <p> end </p> 
+                    <TimePicker 
+                        start={ this.timeConverter(startTime) || "7:30" } 
+                        end="17:00" 
+                        step={30}
+                        value={endTime}
+                        onChange={this.handleEndTimeChange}
+                    />
+                </div>
+            </div>
+            <div className="Makebooking--buttonholder">
+                <button className="Makebooking--button"disabled={bookingButton}  onClick={this.handleBookingRequest}> Make Booking request! </button>
+            </div>
         </div>
     )
   }
