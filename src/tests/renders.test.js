933a87b2a0e1da2da52db0750fa6fad63bf36d74
Fraser Solomon <fmsolomon1@gmail.com>
2018-07-10 17:06:13 +1000

    basic test

---
+++ src/tests/renders.test.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import AppRouter from '../routers/AppRouter.js'
+
+import { shallow } from 'enzyme'
+import './setUpTests'
+
+describe('renders all components without crashing', () => {
+  it('renders App crashing', () => {
+    shallow(<AppRouter />);
+  }) 
+})
