bad7fc99a76471ae2e7ccced42bc5b03e9367af7
Fraser Solomon <fmsolomon1@gmail.com>
2018-07-10 15:44:08 +1000

    home page done

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -4,11 +4,12 @@ import { NavLink } from 'react-router-dom'
 const Nav = () => (
     <div className="Navbar">
         <div className="Navbar--links"> 
-            <NavLink exact to="/"> Home </NavLink>
-            <NavLink exact to="/about"> About </NavLink>
-            <NavLink exact to="/booking"> Make a Booking </NavLink>
-            <NavLink exact to="/contact"> Contact </NavLink>
-            <NavLink exact to="/login"> Login/Signup </NavLink>
+            <NavLink exact to="/"> MW </NavLink>
+            <NavLink exact to="/about"> About Mike </NavLink>
+            <NavLink exact to="/booking"> Make Booking </NavLink>
+            <NavLink exact to="/contact"> Get in Touch </NavLink>
+            <NavLink exact to="/login"> Login </NavLink>
+            <NavLink exact to="/testimonials"> Testimonials </NavLink>
         </div>
     </div>
 )
