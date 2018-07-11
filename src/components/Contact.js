e8a70d73e5d8e7610eebe59c0ed4e39f57611f68
Fraser Solomon <fmsolomon1@gmail.com>
2018-07-12 09:55:56 +1000

    basic input fields, and styling

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,15 +1,24 @@
 import React from 'react';
 
-const Contact = () => (
-    <div className = "contact">
-        <p> Contact Mike  </p>
-        <input placeholder= "firstname" />
-        <input placeholder= "lastname" />
-        <input placeholder= "phone" />
-        <input placeholder= "firstname" />
-        <input placeholder= "artist name" />
-        <input placeholder= "What would you like to talk about?" />
-    </div>
-)
+class Contact extends React.Component{
+
+    contactRequest = () => {
+        console.log('hello')
+    }
+
+    render(){
+        return(
+        <div className = "contact">
+            <p> Contact Mike  </p>
+            <input placeholder = "firstname" />
+            <input placeholder = "lastname" />
+            <input placeholder = "phone" />
+            <input placeholder = "artist/band name" />
+            <input placeholder = "What would you like to talk about?" />
+            <button onClick={this.contactRequest}> send </button>
+        </div>
+        )
+    }
+}
  
 export default Contact;
