7eef8a4caa5f20597e00fd6ce76eccdff4e5c145
Fraser Solomon <fmsolomon1@gmail.com>
2018-07-11 14:29:32 +1000

    pulls in api data, approved, decline, pending all accurately rendered

--- src/components/AllBookings.js
+++ src/components/AllBookings.js
@@ -1,75 +1,160 @@
 import React from 'react';
 import orderBy from 'lodash.orderby' // one function from lodash library.
 import moment from 'moment'
+import axios from 'axios'
 
-// import data from './dummyBookings.json' // will be replaced by a get request, and put into componentWillMount.
-
-const bookingsData = orderBy(data.bookings, (o) => {
-    return new moment(o.date).format('YYYYMMDD')
-}) // this arranges the bookings by data, most recent data is first.
-// buttons arent on the right bookings, they work when they arent order like below.
-
-// const bookingsData = data.bookings //this 
 
 class AllBookings extends React.Component {
     state = { 
-        bookings: bookingsData
+        bookings: [],
+        declinedBookings: [],
+        approvedBookings: [],
+        pendingBookings: [],
+        completedBookings: []
      }
-            // We just need to post the updated object to db in this function, same with declined.
-        handleApprovedBooking = (bookingID) => { 
-            let bookingsCopy = bookingsData
 
-            bookingsCopy.forEach((obj) => {
-                if(obj.clientId === bookingID){
-                    obj.bookingStatus = 'approved'
-                }
-            })
-            this.setState({bookings: bookingsCopy})
-        }
+     componentWillMount(){
 
-        componentWillMount(){
-            const data = {}
-                data.bookings = axios.get("https://mikewserver.herokuapp.com/bookings")
-                .then((response) => {
-                    console.log(response)
+        axios.get("https://mikewserver.herokuapp.com/bookings")
+            .then((response) => {
+                let bookings = orderBy(response.data, (o) => {
+                    return new moment(o.date).format('YYYYMMDD')
+                })
+                console.log(response.data)
+                response.data.forEach((data) => {
+                    this.checkBookingStatus(data)
                 })
 
+            this.setState({ bookings }, () => { console.log(this.state) })
+        })
+    }
+
+        checkBookingStatus = (booking) => {
+            if(booking.bookingStatus === "approved"){
+                this.setState({ approvedBookings: this.state.approvedBookings.concat(booking) })
+            } else if(booking.bookingStatus === "declined") {
+                this.setState({ declinedBookings: this.state.declinedBookings.concat(booking) })
+            } else if(booking.bookingStatus === "pending"){
+                this.setState({ pendingBookings: this.state.pendingBookings.concat(booking) })
+                console.log('pending')
+            } else if(booking.bookingStatus === "completed"){
+                this.setState({ completedBookings: this.state.completedBookings.concat(booking) })
+            }
         }
 
-        handleDeclineBooking = (bookingID) => { 
-            let bookingsCopy = this.state.bookings
+
+        handleApprovedBooking = (bookingID) => { 
+
+            let bookingsCopy = this.state.pendingBookings
+            let approved = []
 
             bookingsCopy.forEach((obj) => {
                 if(obj.clientId === bookingID){
-                    obj.bookingStatus = 'declined'
+                    obj.bookingStatus = "approved"
+                    approved.push(obj)
                 }
             })
-            this.setState({bookings: bookingsCopy})
+
+            bookingsCopy = bookingsCopy.filter((obj) => { 
+                return obj.bookingStatus !== "approved";
+            })
+
+            this.setState({
+                        approvedBookings: approved,
+                        pendingBookings: bookingsCopy
+                        })
         }
 
-        addTwo = (num,num2) => {
-            return num + num2
+
+        handleDeclineBooking = (bookingID) => { 
+                
+            let bookingsCopy = this.state.pendingBookings
+            let declined = []
+
+            bookingsCopy.forEach((obj) => {
+                if(obj.clientId === bookingID){
+                    obj.bookingStatus = "declined"
+                    declined.push(obj)
+                }
+            })
+
+            bookingsCopy = bookingsCopy.filter((obj) => { 
+                return obj.bookingStatus !== "declined";
+            })
+
+            this.setState({
+                        declinedBookings: declined,
+                        pendingBookings: bookingsCopy
+                        })
         }
+
 
     render() { 
-        console.log(this.state.bookings)
-        const { bookings } = this.state
+        const { completedBookings, declinedBookings, approvedBookings, pendingBookings } = this.state
         return ( 
             <div>
-            <h1> All Bookings </h1>
+            <h1> Pending Bookings </h1>
             {
-                bookings.map((booking) => {
+                pendingBookings ? 
+                pendingBookings.map((booking) => {
                     return (
                         <div key={booking.clientId}> 
                             <h3> date: {moment(booking.date, 'YYYYMMDD').format('MMM Do YY')  /* reformats into a more readable date. */} </h3> 
                             <p> starts: {booking.startTime} </p>
                             <p> ends: {booking.endTime} </p>
                             <p> booking status: {booking.bookingStatus} </p>
+
                             <button className="approve-button" onClick={() => this.handleApprovedBooking(booking.clientId)}> Approve Booking? </button>
                             <button onClick={() => this.handleDeclineBooking(booking.clientId)}> Decline Booking? </button>
                         </div>
                     )
                 })
+            : <p> loading hollup.. </p>
+            }
+            <h1> Approved Bookings </h1>
+            {
+                approvedBookings ? 
+                approvedBookings.map((booking) => {
+                    return (
+                        <div key={booking.clientId}> 
+                            <h3> date: {moment(booking.date, 'YYYYMMDD').format('MMM Do YY')  /* reformats into a more readable date. */} </h3> 
+                            <p> starts: {booking.startTime} </p>
+                            <p> ends: {booking.endTime} </p>
+                            <p> booking status: {booking.bookingStatus} </p>
+                        </div>
+                    )
+                })
+            : <p> loading hollup.. </p>
+            }
+            <h1> Declined Bookings </h1>
+            {
+                declinedBookings ? 
+                declinedBookings.map((booking) => {
+                    return (
+                        <div key={booking.clientId}> 
+                            <h3> date: {moment(booking.date, 'YYYYMMDD').format('MMM Do YY')  /* reformats into a more readable date. */} </h3> 
+                            <p> starts: {booking.startTime} </p>
+                            <p> ends: {booking.endTime} </p>
+                            <p> booking status: {booking.bookingStatus} </p>
+                        </div>
+                    )
+                })
+            : <p> loading hollup.. </p>
+            }
+            <h1> Completed Bookings </h1>
+            {
+                completedBookings ? 
+                completedBookings.map((booking) => {
+                    return (
+                        <div key={booking.clientId}> 
+                            <h3> date: {moment(booking.date, 'YYYYMMDD').format('MMM Do YY')  /* reformats into a more readable date. */} </h3> 
+                            <p> starts: {booking.startTime} </p>
+                            <p> ends: {booking.endTime} </p>
+                            <p> booking status: {booking.bookingStatus} </p>
+                        </div>
+                    )
+                })
+            : <p> loading hollup.. </p>
             }
             </div>
          )
