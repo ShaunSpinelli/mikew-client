6f80f24c5f4d1b57e73643944e040e8f919fbe9c
Caro Evans <caroline.w.evans@gmail.com>
2018-07-16 17:34:59 +1000

    rearranged nav

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -4,15 +4,12 @@ import { NavLink } from 'react-router-dom'
 const Nav = () => (
     <div className="Navbar">
         <div className="Navbar--links"> 
-
             <NavLink exact to="/"> MW </NavLink>
-            <NavLink exact to="/about"> About Mike </NavLink>
-            <NavLink exact to="/booking"> Make Booking </NavLink>
-            <NavLink exact to="/contact"> Get in Touch </NavLink>
-            <NavLink exact to="/login"> Login </NavLink>
+            <NavLink exact to="/about"> About Michael </NavLink>
+            <NavLink exact to="/contact"> Contact </NavLink>
+            <NavLink exact to="/booking"> Book an Appointment </NavLink>
             <NavLink exact to="/admin"> Admin </NavLink>
-            <NavLink exact to="/testimonials"> Testimonials </NavLink>
-
+            <NavLink exact to="/login"> Login </NavLink>
         </div>
     </div>
 )
