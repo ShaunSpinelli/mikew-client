86093747bac71ab4be05be98b7799db2724b5038
Fraser Solomon <fmsolomon1@gmail.com>
2018-07-18 08:59:16 +1000

    cleanup register

--- src/components/Register.js
+++ src/components/Register.js
@@ -19,6 +19,10 @@ import { api } from '../api/init'
     openModal = () => {
         this.setState({modalIsOpen: true});
     }
+
+    closeModal = (e) => {
+        this.setState({modalIsOpen: false});
+    }
    
     handleChange = (e) => {
         this.setState({ [e.target.name]: e.target.value })
@@ -36,39 +40,35 @@ import { api } from '../api/init'
             Role: "user",
             profileImg: "url"
         }
-        console.log(newUser)
         api.post('users/register', newUser)
         .then(res => { localStorage.setItem('token',res.data.token)
-        this.props.updatedLogIn()
-        this.closeModal()})
+            this.props.updatedLogIn()
+            this.closeModal()})
         .catch(err => this.setState({loginError: true}))
     }
 
-    closeModal = (e) => {
-        this.setState({modalIsOpen: false});
-    }
-   
     render() {
+        const { openModal, closeModal, handleChange, handleRegister } = this
       return (
         <Fragment>
-            <button className="register--button" onClick={this.openModal}>Register</button>
+            <button className="register--button" onClick={openModal}>Register</button>
             <Modal
                 className="modal fade modal-dialog loginmodal-container"
                 id="login-modal"
                 isOpen={this.state.modalIsOpen}
-                onRequestClose={this.closeModal}
+                onRequestClose={closeModal}
                 contentLabel="Example Modal"
             >
-                <button  onClick={this.closeModal}>close</button>
+                <button  onClick={closeModal}>close</button>
                 <h1>Register Your Account</h1><br />
                 <p>{this.state.loginError ? 'Invalid Login Details': ''}</p>
                 <form>
-                    <input onChange={this.handleChange} name="firstName" placeholder="first name" />
-                    <input onChange={this.handleChange} name="lastName" placeholder="last name" />
-                    <input onChange={this.handleChange} name="phoneNumber" placeholder="phone number" />
-                    <input onChange={this.handleChange} type="email" name="email" placeholder="email" />
-                    <input onChange={this.handleChange} type="password" name="password" placeholder="password" />
-                    <input onClick={this.handleRegister} type="submit" name="register" className="login loginmodal-submit" value="Register" />
+                    <input onChange={handleChange} name="firstName" placeholder="first name" />
+                    <input onChange={handleChange} name="lastName" placeholder="last name" />
+                    <input onChange={handleChange} name="phoneNumber" placeholder="phone number" />
+                    <input onChange={handleChange} type="email" name="email" placeholder="email" />
+                    <input onChange={handleChange} type="password" name="password" placeholder="password" />
+                    <input onClick={handleRegister} type="submit" name="register" className="login loginmodal-submit" value="Register" />
                 </form>
                 <div className="login-help">
                     <a href="#">Login</a> - <a href="#">Forgot Password</a>
