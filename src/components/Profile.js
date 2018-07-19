9634ce57b9f039f44ca44e256032e34bb540eef3
Caro Evans <caroline.w.evans@gmail.com>
2018-07-19 14:39:59 +1000

    profile component with stylin'

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,38 +1,50 @@
 import React, { Component, Fragment } from 'react'
 import { api } from '../api/init'
 import Loading from './Loading'
+import TextField from 'material-ui/TextField'
 
 const jwtDecode = require('jwt-decode')
 
 class Profile extends Component {
     state = {
         loading: false,
-        id: ""
+        id: "",
+        firstName:"",
+        lastName:"",
+        email:"",
+        phoneNumber:"",
+        profileImg:""
     }
 
     componentDidMount(){
         this.setState( {loading: true} )
         const decoded = jwtDecode(localStorage.getItem('token'))
         this.getUserProfile(decoded.sub)
-        console.log(decoded.sub)
     }
 
     getUserProfile = (id) => {
         api.get("users/id", {params : {id: id }})
         .then((response) => {
-            console.log(response)
-        this.setState({id, loading: false }) 
-        console.log(this.state.id)})
+        const {id, firstName, lastName, email, phoneNumber, profileImg} = response.data
+        this.setState(
+            {id, firstName, lastName, email, phoneNumber, profileImg, loading: false })})
+        // console.log(res.data.firstName)})
         .catch((err) => { console.log(err) })
         }
 
     render () {
-        const { loading } = this.state
+        const { loading, firstName, lastName, email, phoneNumber, profileImg } = this.state
+        const fullName = `${firstName} ${lastName}`
         { if(loading) {return <Loading className = "loadingScreen" /> }}
         return (
-            <Fragment>
-                <p>Hello</p>
-            </Fragment>
+            <div className="profile--holder">
+                <div className="profile">
+                    <img className="profilepic" src="https://eadb.org/wp-content/uploads/2015/08/profile-placeholder-300x300.jpg" />
+                    <h1 className="profile--title"> {fullName} </h1>
+                    <p className="profile">Email: { email } </p>
+                    <p>Phone: { phoneNumber } </p>
+                </div>
+            </div>
         )
     }
 
