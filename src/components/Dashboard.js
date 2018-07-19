4e7841a989fd194cc6416605cc83bbbd783685d7
Shaun Spinelli <shaun.spinelli@gmail.com>
2018-07-19 11:29:30 +1000

    created dash board component

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -2,9 +2,21 @@ import React from 'react'
 import AllBookings from '../components/AllBookings.js'
 import AllContacts from '../components/AllContacts.js'
 import AllUsers from '../components/AllUsers.js'
+const jwtDecode = require('jwt-decode')
 
-
+const decoded = jwtDecode(localStorage.getItem('token'))
 
 export default () =>(
-    <h1></h1>
+    <div>
+        <h1>DashBoard</h1>
+        <p>Bookings</p>
+        <AllBookings />
+        {decoded.role === 'admin' && 
+        <div>
+            <p>Contact Requests</p>
+             <AllContacts />
+        </div>
+    
+    }
+    </div>
 )
