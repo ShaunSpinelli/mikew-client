0e0421ae5426f61e4df687f8ae9032ea7ad94eda
Caro Evans <caroline.w.evans@gmail.com>
2018-07-19 11:29:43 +1000

    profile component

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,13 +1,42 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
+import { api } from '../api/init'
+import Loading from './Loading'
 
 const jwtDecode = require('jwt-decode')
-jwtDecode(localStorage.getItem('token'))
 
+class Profile extends Component {
+    state = {
+        loading: false,
+        id:''
+    }
 
-const Profile = () => {
-    return (
-        <p>My profile</p>
-    )
+    componentDidMount(){
+        this.setState( {loading: true} )
+        const decoded = jwtDecode(localStorage.getItem('token'))
+        this.getUserProfile(decoded.sub)
+        console.log(decoded.sub)
+    }
+
+    getUserProfile = (id) => {
+        api.get("users/id", {id: 'zz5b4ecaab3d9c6d403c1ea730' })
+        .then((response) => {
+            console.log(response)
+        this.setState({ id, loading: false }) })
+        .catch((err) => { console.log(err) })
+        }
+
+    render () {
+        const { loading } = this.state
+        { if(loading) {return <Loading className = "loadingScreen" /> }}
+        return (
+            <Fragment>
+                <p>Hello</p>
+            </Fragment>
+        )
+    }
+
 }
+
+
 
 export default Profile;
