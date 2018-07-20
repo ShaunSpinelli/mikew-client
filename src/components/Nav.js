95a91b9e55ac5c6d37db63a19e3d17a19b2d1cfe
Fraser Solomon <fmsolomon1@gmail.com>
2018-07-20 14:10:51 +1000

    navbar touching

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -3,28 +3,54 @@ import { NavLink } from 'react-router-dom'
 import Login from './Login'
 import Register from './Register'
 
-const Nav = (props)=> {
+class Nav extends React.Component {
+    // state ={ 
+    //     scrollingLock: false
+    // }
 
-    return (
-        <div className="Navbar">
-            <div className="Navbar--links"> 
-                <NavLink exact to="/"> MW </NavLink>
-                <NavLink exact to="/"> About Michael </NavLink>
-                <NavLink exact to="/contact"> Contact </NavLink>
-                <NavLink exact to="/booking"> Book an Appointment </NavLink>
-                { localStorage.token ? 
+    // componentDidMount(){
+    //     window.addEventListener('scroll', this.handleScroll);
+    // }
+    
+    // componentWillUnmount() {
+    //     window.removeEventListener('scroll', this.handleScroll);
+    // }
+    
+    // handleScroll = () => {
+
+    //     if (window.scrollY > 100) {
+    //       this.setState({
+    //         scrollingLock: true
+    //       });
+    //     } else if (window.scrollY < 100) {
+    //       this.setState({
+    //         scrollingLock: false
+    //       });
+    //     }
+    // }
+
+    render(){
+        return (
+            <div className="Navbar">
+                <div className="Navbar--links"> 
+                    <NavLink exact to="/"> MW </NavLink>
+                    <NavLink exact to="/"> About Michael </NavLink>
+                    <NavLink exact to="/contact"> Contact </NavLink>
+                    <NavLink exact to="/booking"> Book an Appointment </NavLink>
+                    { localStorage.token ? 
+                        <React.Fragment> 
+                            <NavLink exact to="/profile"> Profile </NavLink>
+                            <NavLink to="/" onClick={this.props.logout}> Logout </NavLink> 
+                        </React.Fragment>
+                    :  
                     <React.Fragment> 
-                        <NavLink exact to="/profile"> Profile </NavLink>
-                        <NavLink to="/" onClick={props.logout}> Logout </NavLink> 
-                    </React.Fragment>
-                :  
-                <React.Fragment> 
-                    <Login updatedLogIn={props.updatedLogIn}/> 
-                    <Register /> 
-                </React.Fragment> }
+                        <Login updatedLogIn={this.props.updatedLogIn}/> 
+                        <Register /> 
+                    </React.Fragment> }
+                </div>
             </div>
-        </div>
-    )
+        )
+    }
 }
 
  
