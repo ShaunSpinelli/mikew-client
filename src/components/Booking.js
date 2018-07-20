c4edca94cba2eea198f4167ca6dea4398ebf7eaf
Shaun Spinelli <shaun.spinelli@gmail.com>
2018-07-20 16:40:18 +1000

    fixed error

--- src/components/Booking.js
+++ src/components/Booking.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Booking = (props) => {
     return ( 
         <div key = {props._id}> 
-            <p> Date: {props.date} Starts: {props.startTime} Ends: {props.endTime} Booking status: {props.bookingStatus} <Note: {props.info} </p>
+            <p> Date: {props.date} Starts: {props.startTime} Ends: {props.endTime} Booking status: {props.bookingStatus} Note: {props.info} </p>
          </div>
      )
 }
