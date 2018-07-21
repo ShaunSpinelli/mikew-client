4ad001639c1342bd4dca936df8d9448ed02ef89d
Caro Evans <caroline.w.evans@gmail.com>
2018-07-21 11:33:33 +1000

    styled homepg with SC and removed CSS file

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -2,7 +2,7 @@ import React, {Fragment} from 'react'
 import Map from './Map'
 import Testimonies from './Testimonies.js'
 import Footer from './Footer.js'
-import { Title, Subtitle, Slogan, Home, Icon } from './HomePage.styles';
+import { Title, Subtitle, Slogan, Home, Icon1, Icon2 } from '../styles/HomePage.styles';
 
 
 const HomePage = () => (
@@ -12,8 +12,8 @@ const HomePage = () => (
             <Subtitle> MUSIC PRODUCTION </Subtitle>
             <Slogan> Professional Producing Assistance <br />
                 Consultancy, Producing Quality Music & Technical Help </ Slogan>
-                <Icon src="http://icons.iconarchive.com/icons/xenatt/minimalism/256/App-SoundCloud-icon.png" alt="soundcload logo" />
-                <Icon src="https://www.shareicon.net/data/256x256/2015/08/29/92757_like_606x606.png" alt="facebook logo"/>
+                <Icon1 src="http://icons.iconarchive.com/icons/xenatt/minimalism/256/App-SoundCloud-icon.png" alt="soundcloud logo" />
+                <Icon2 src="https://www.shareicon.net/data/256x256/2015/08/29/92757_like_606x606.png" alt="facebook logo"/>
         </ Home>
         <Testimonies />
         <Map />
---
+++ src/styles/HomePage.styles.js
@@ -0,0 +1,43 @@
+import styled, { css, injectGlobal } from 'styled-components'
+
+const Home = styled.div`
+  height: 50vh;
+`
+const setTextProps = (size, weight, colour, hovercolour ) => css`
+  font-size: ${size};
+  font-weight: ${weight};
+  color: ${colour};
+  margin: 0;
+  &:hover {
+    color:${hovercolour}
+  }
+`
+const setIconProps = (position, paddingr, bottom, right, height) => css`
+  position: ${position};
+  padding-right: ${paddingr}
+  bottom: ${bottom};
+  right: ${right};
+  height: ${height};
+`
+
+const Title = styled.h4`
+padding-top: 0.5em;
+  ${setTextProps('3.5em','300','yellow', 'white')}
+`
+const Subtitle = styled.h2`
+padding: 0.75em;
+${setTextProps('2em','300','white', 'yellow')}
+`
+const Slogan = styled.h3`
+line-height: 1.5em;
+padding-bottom: 0.5em;
+${setTextProps('1.5em','300','white', 'yellow')}
+`
+const Icon1 = styled.img`
+${setIconProps('absolute', '5vw', '1rem', '3rem', '2.5rem')}
+`
+const Icon2 = styled.img`
+${setIconProps('absolute', '5vw', '1rem', '0rem', '2.5rem')}
+`
+
+export { Title, Subtitle, Slogan, Home, Icon1, Icon2 }
