c59f1eb2f618f7496f8435858191f27f702b2f2e
Fraser Solomon <fmsolomon1@gmail.com>
2018-07-25 11:57:44 +1000

    images resized

--- src/components/Testimonies.js
+++ src/components/Testimonies.js
@@ -1,4 +1,9 @@
 import React from 'react'
+import beatles from '../images/testimonies/beatles.jpg'
+import tameImpala from '../images/testimonies/tame-impala.jpg'
+import ladySong from '../images/testimonies/lady-songwriter.jpg'
+import jimi from '../images/testimonies/jimi.jpg'
+import doggo from '../images/testimonies/doggo.jpg'
 import TestimonieSingle from './TestimonieSingle'
 import guitar from '../images/guitar-pic.jpeg'
 import { Testimonials, Title } from '../styles/cssInJs/Testimonials.styles';
@@ -9,39 +14,34 @@ const Testimonies = () => {
             <Title> Testimonials </ Title>
             <div className="scrolling-wrapper">
                     <TestimonieSingle // renders testomie signle format for all testimonies.
-                        image= "https://static-wix-blog.wix.com/blog/wp-content/uploads/2016/01/07101455/5-ways-musicians-can-make-more-money_image.png"
+                        image= {beatles}
                         testimonie= "'' Dunn was the guy most responsible for putting a face on (Manitoba Television Network’s) first on-air decade. '' "
                         author= "- The Beatles"
                         />
                     <TestimonieSingle 
-                        image= "https://media.pitchfork.com/photos/5929a47a5e6ef959693208a6/1:1/w_600/0b253d77.jpg"
+                        image= {tameImpala}
                         testimonie= "'' Kevin likes to be the centre of attention. He’s one of those guys that doesn’t demand the attention, but just falls into or earns the role of head attention getter. ''' "
                         author= "- Jimi Hendrix"
                         />
                     <TestimonieSingle 
-                        image= "https://www.billboard.com/files/styles/article_main_image/public/media/Tame-Impala-Lollapalooza-Berlin-2015-billboard-650.jpg"
+                        image= {ladySong}
                         testimonie= "'' Kevin has the unique ability to communicate and project a positive approach in everything he does.  A a true people-person…with high level social skills like convincing, negotiating, selling, communicating ideas, telling stories,motivating… '' "
                         author= "- King"
                         />
                     <TestimonieSingle 
-                        image= "http://somethingelsereviews.com/wp-content/uploads/2013/05/Queen-Freddie-Mercury-Brian-May.jpg"
+                        image= {jimi}
                         testimonie= "'' He spits out these clever, poetic pieces of art faster then anyone else I have ever come across.' -Tony Wytinck, Senior Editor, MidCanada Production Services. ''"
                         author= "- Nacho Man"
                         />
                     <TestimonieSingle 
-                        image= "https://www.rollingstone.com/wp-content/uploads/2018/06/rs-197621-84894709.jpg?crop=900:600&width=440"
+                        image= {doggo}
                         testimonie= " '' He has used his entertainment skills to bring people together, network, celebrate special occasions, raise money for organizations and charities, market himself and his work, and to create great memories for all around him. ''"
                         author= "- Dennis"
                         />
                     <TestimonieSingle 
-                        image= "http://zurlocker.typepad.com/photos/uncategorized/cimg1148.jpg"
+                        image= {jimi}
                         testimonie= "'' Kevin likes to be the centre of attention. He’s one of those guys that doesn’t demand the attention, but just falls into or earns the role of head attention getter. ''"
                         author= "- Dr legend kid"
-                        />
-                    <TestimonieSingle 
-                        image= "http://images6.fanpop.com/image/photos/33500000/funny-guitar-msyugioh123-33511026-400-300.jpg"
-                        testimonie= "'' Kevin will win over a crowd with a clever invitations, organizing of gatherings, re-written song lyrics, stories of the past or performing of songs that everyone can sing along. ''"
-                        author= "- Cat"
                         />
             </div>
         </ Testimonials >
